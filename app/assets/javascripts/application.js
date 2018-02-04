b956590804934f3e47f21414085daeaac544dade
sandip <bhowmick.sandip@gmail.com>
2018-02-04 22:59:09 +0530

    add template and add device

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
-//= require_tree .
+//= require jquery.easing.min
+//= require scrollreveal
+//= require jquery.magnific-popup.min
+
+
